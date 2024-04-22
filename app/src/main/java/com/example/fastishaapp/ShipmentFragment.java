6e15b143b0389f9cd0d034c6f2f478fbfec42b50
Michael ndolo <michaelndolo17142@gmail.com>
2024-04-23 02:07:18 +0300

    designing a shipment UI

--- app/src/main/java/com/example/fastishaapp/ShipmentFragment.java
+++ app/src/main/java/com/example/fastishaapp/ShipmentFragment.java
@@ -1,58 +1,24 @@
 package com.example.fastishaapp;
 
 import android.os.Bundle;
-
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
-
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link ShipmentFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
 public class ShipmentFragment extends Fragment {
-
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
-
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
 
     public ShipmentFragment() {
         // Required empty public constructor
     }
 
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment ShipmentFragment.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static ShipmentFragment newInstance(String param1, String param2) {
-        ShipmentFragment fragment = new ShipmentFragment();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
-    }
-
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
+    public static ShipmentFragment newInstance() {
+        return new ShipmentFragment();
     }
 
     @Override
@@ -61,4 +27,22 @@ public class ShipmentFragment extends Fragment {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_shipment, container, false);
     }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        Button confirm = view.findViewById(R.id.btnConfirm);
+        confirm.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                EditText edtItem = view.findViewById(R.id.edtItem);
+                TextView txtItem = view.findViewById(R.id.txtItem);
+                String itemText = edtItem.getText().toString();
+                txtItem.setText(itemText);
+            }
+        });
+    }
+
+    // Removed unused method
+
 }
