0323cf5f19bdca8a7fc6c13faf5c151f28d84c5d
Michael ndolo <michaelndolo17142@gmail.com>
2024-04-25 02:27:03 +0300

    adding and enable geo location

--- app/src/main/java/com/example/fastishaapp/ShipmentFragment.java
+++ app/src/main/java/com/example/fastishaapp/ShipmentFragment.java
@@ -1,8 +1,19 @@
 package com.example.fastishaapp;
 
+import static android.content.Context.LOCATION_SERVICE;
+
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.location.Address;
+import android.location.Geocoder;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
 import android.os.Bundle;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -11,7 +22,13 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 
-public class ShipmentFragment extends Fragment {
+import java.util.List;
+import java.util.Locale;
+
+public class ShipmentFragment extends Fragment implements LocationListener {
+
+    private LocationManager locationManager;
+    private TextView fromLocation;
 
     public ShipmentFragment() {
         // Required empty public constructor
@@ -32,9 +49,19 @@ public class ShipmentFragment extends Fragment {
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
         Button confirm = view.findViewById(R.id.btnConfirm);
+        fromLocation = view.findViewById(R.id.txtFromLocation);
+
+        if (ContextCompat.checkSelfPermission(requireContext(), Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(requireActivity(), new String[]{
+                    Manifest.permission.ACCESS_FINE_LOCATION
+            }, 100);
+        }
+
         confirm.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                getLocation();
+
                 EditText edtItem = view.findViewById(R.id.edtItem);
                 EditText editDescription = view.findViewById(R.id.edtDescription);
 
@@ -47,6 +74,58 @@ public class ShipmentFragment extends Fragment {
         });
     }
 
-    // Removed unused method
+    private void getLocation() {
+        try {
+            locationManager = (LocationManager) requireContext().getSystemService(LOCATION_SERVICE);
+            locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 5000, 5, this);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
 
+    @Override
+    public void onLocationChanged(@NonNull Location location) {
+        try {
+            Geocoder geocoder = new Geocoder(requireContext(), Locale.getDefault());
+            List<Address> addresses = geocoder.getFromLocation(location.getLatitude(), location.getLongitude(), 1);
+            String address = addresses.get(0).getAddressLine(0);
+
+            fromLocation.setText(address);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Override
+    public void onLocationChanged(@NonNull List<Location> locations) {
+        LocationListener.super.onLocationChanged(locations);
+    }
+
+    @Override
+    public void onFlushComplete(int requestCode) {
+        LocationListener.super.onFlushComplete(requestCode);
+    }
+
+    @Override
+    public void onStatusChanged(String provider, int status, Bundle extras) {
+        LocationListener.super.onStatusChanged(provider, status, extras);
+    }
+
+    @Override
+    public void onProviderEnabled(@NonNull String provider) {
+        LocationListener.super.onProviderEnabled(provider);
+    }
+
+    @Override
+    public void onProviderDisabled(@NonNull String provider) {
+        LocationListener.super.onProviderDisabled(provider);
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        if (requestCode == 100 && grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+            getLocation();
+        }
+    }
 }
