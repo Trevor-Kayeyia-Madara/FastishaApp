c34b5b2a874547d1f2864d77bf21754c6bf03a12
Trevor-Kayeyia-Madara <trevor.madara@student.moringaschool.com>
2024-04-26 14:53:15 +0300

    git commit -m "FASTISHA-16 <I have created fragments for the Admin>"

--- app/src/main/java/com/example/fastishaapp/Admin.java
+++ app/src/main/java/com/example/fastishaapp/Admin.java
@@ -4,21 +4,43 @@ import android.os.Bundle;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
+
+import com.example.fastishaapp.databinding.ActivityMainBinding;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class Admin extends AppCompatActivity {
-
+    ActivityMainBinding binding;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        replaceFragment(new BackFragment());
         setContentView(R.layout.activity_admin);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
+
+        BottomNavigationView bottomNavigationView = findViewById(R.id.bottomNavigationView2);
+        bottomNavigationView.setOnItemSelectedListener(menuItem -> {
+            int itemId =menuItem.getItemId();
+
+            if(itemId == R.id.back){
+                replaceFragment(new BackFragment());
+            }else if(itemId == R.id.shipment){
+                replaceFragment(new AgentFragment());
+            }else if(itemId == R.id.list) {
+                replaceFragment(new ListFragment());
+            }else if(itemId == R.id.logout) {
+                replaceFragment(new LogOutFragment());
+            }
+            return true;
         });
+    }
+    private void replaceFragment(Fragment fragment){
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+        fragmentTransaction.replace(R.id.frame_layout, fragment);
+        fragmentTransaction.commit();
     }
 }
