dc006a625f57413fd06099e34f80e92c470620d5
Michael ndolo <105848178+Mikebabu254@users.noreply.github.com>
2024-10-08 08:44:40 +0300

    designing the receipt.xml

--- app/src/main/java/com/example/fastishaapp/PaymentFragment.java
+++ app/src/main/java/com/example/fastishaapp/PaymentFragment.java
@@ -5,6 +5,8 @@ import android.os.Bundle;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.view.LayoutInflater;
 import android.view.View;
@@ -19,6 +21,7 @@ import android.widget.TextView;
 public class PaymentFragment extends Fragment {
 
     TextView itemType, txtDescription, txtFromLocation, txtToLocation, txtPrice;
+    RecyclerView recyclerView;
 
     public PaymentFragment() {
         // Required empty public constructor
@@ -57,6 +60,8 @@ public class PaymentFragment extends Fragment {
             String description = bundle.getString("description");
             String fromLocation = bundle.getString("fromLocation");
             String toLocation = bundle.getString("toLocation");
+            RecyclerView recyclerView = view.findViewById(R.id.myRecyleView);
+            recyclerView.setLayoutManager(new LinearLayoutManager(getActivity()));
 
             itemType.setText(item);
             txtDescription.setText(description);
