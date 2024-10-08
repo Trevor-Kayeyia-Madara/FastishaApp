91680d571a4b6e7036e480a738c12b29dd785f3b
Michael ndolo <105848178+Mikebabu254@users.noreply.github.com>
2024-10-08 13:45:13 +0300

    encounter some errors 3

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -56,6 +56,5 @@ dependencies {
     testImplementation(libs.junit)
     androidTestImplementation(libs.ext.junit)
     androidTestImplementation(libs.espresso.core)
-    implementation(libs.firebase.ui.auth)
 
 }
