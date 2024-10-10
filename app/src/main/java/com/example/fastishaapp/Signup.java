faa88568402d5fae2e1cfec1ade94502547fee3c
Michael ndolo <105848178+Mikebabu254@users.noreply.github.com>
2024-10-10 08:14:02 +0300

    moving from Signup to login

--- app/src/main/java/com/example/fastishaapp/Signup.java
+++ app/src/main/java/com/example/fastishaapp/Signup.java
@@ -1,6 +1,10 @@
 package com.example.fastishaapp;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -9,16 +13,21 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class Signup extends AppCompatActivity {
+    EditText firstName, email, phoneNumber, password, confirmPassword;
+    Button signUp;
+    TextView redirectLogin;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_signup);
-//        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-//            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-//            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-//            return insets;
-//        });
+
+        redirectLogin = findViewById(R.id.redirectLogin);
+
+        redirectLogin.setOnClickListener(view -> {
+            Intent intent = new Intent(Signup.this, Login.class);
+            startActivity(intent);
+        });
     }
 }
