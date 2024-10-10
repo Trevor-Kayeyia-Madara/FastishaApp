baf1e9aadcf3af82832bcc0e2861d7236e92b20d
Michael ndolo <105848178+Mikebabu254@users.noreply.github.com>
2024-10-10 17:36:04 +0300

    shipment intent

--- app/src/main/java/com/example/fastishaapp/HomePage.java
+++ app/src/main/java/com/example/fastishaapp/HomePage.java
@@ -1,6 +1,8 @@
 package com.example.fastishaapp;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.LinearLayout;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -9,16 +11,24 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class HomePage extends AppCompatActivity {
+    LinearLayout shipmentLayer;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_home_page);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
+//        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+//            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+//            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+//            return insets;
+//        });
+
+        shipmentLayer = findViewById(R.id.shipmentLayer);
+
+        shipmentLayer.setOnClickListener(v -> {
+            Intent intent = new Intent(HomePage.this, Shipment.class);
+            startActivity(intent);
         });
     }
 }
