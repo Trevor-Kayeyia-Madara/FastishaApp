68a1b9f3cd280d18b0e70754b37fe0da4efb4ba5
Michael ndolo <105848178+Mikebabu254@users.noreply.github.com>
2024-10-10 08:46:26 +0300

    didnt manage to sign in the users

--- app/src/main/java/com/example/fastishaapp/Login.java
+++ app/src/main/java/com/example/fastishaapp/Login.java
@@ -9,14 +9,21 @@ import android.widget.ProgressBar;
 import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 public class Login extends AppCompatActivity {
     Button login, signup;
@@ -75,5 +82,38 @@ public class Login extends AppCompatActivity {
 
     private void signup(String myEmail, String myPassword) {
         progressBar.setVisibility(View.VISIBLE);
+        mAuth.signInWithEmailAndPassword(myEmail,myPassword).addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+            @Override
+            public void onComplete(@NonNull Task<AuthResult> task) {
+                progressBar.setVisibility(View.GONE);
+                if (task.isSuccessful()) {
+                    Toast.makeText(Login.this, "Sign in successful", Toast.LENGTH_SHORT).show();
+                    String userID = mAuth.getCurrentUser().getUid();
+                    mRef.child("users").child(userID).addListenerForSingleValueEvent(new ValueEventListener() {
+                        @Override
+                        public void onDataChange(@NonNull DataSnapshot snapshot) {
+                            if(snapshot.exists()){
+                                String role = snapshot.child("role").getValue(String.class);
+                                if("admin".equals(role)){
+                                    
+                                } else if ("agent".equals(role)) {
+                                    
+                                }else {
+                                    Intent intent = new Intent(Login.this, HomePage.class);
+                                    startActivity(intent);
+                                }
+                            }
+                        }
+
+                        @Override
+                        public void onCancelled(@NonNull DatabaseError error) {
+
+                        }
+                    });
+                } else {
+                    Toast.makeText(Login.this, "Sign in failed", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
     }
 }
