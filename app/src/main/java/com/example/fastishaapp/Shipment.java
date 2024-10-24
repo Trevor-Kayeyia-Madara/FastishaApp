9ce015b41c1276f84c1d7f8a9a5511a732228fd6
Michael ndolo <105848178+Mikebabu254@users.noreply.github.com>
2024-10-24 12:37:02 +0300

    adding permisson checker

--- app/src/main/java/com/example/fastishaapp/Shipment.java
+++ app/src/main/java/com/example/fastishaapp/Shipment.java
@@ -161,6 +161,16 @@ public class Shipment extends AppCompatActivity {
 
     // Get current location using FusedLocationProviderClient
     private void getCurrentLocation() {
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            // TODO: Consider calling
+            //    ActivityCompat#requestPermissions
+            // here to request the missing permissions, and then overriding
+            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+            //                                          int[] grantResults)
+            // to handle the case where the user grants the permission. See the documentation
+            // for ActivityCompat#requestPermissions for more details.
+            return;
+        }
         fusedLocationClient.getLastLocation()
                 .addOnSuccessListener(this, location -> {
                     if (location != null) {
